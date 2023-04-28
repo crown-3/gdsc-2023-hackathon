991d3f939382f8a2fd5c2b2dbf1d26c30c45196c
Shimsuyeon <suyeonshim@gm.gist.ac.kr>
2023-04-29 01:51:51 +0900

    navBar_practice

--- src/Welcome/Welcome.tsx
+++ src/Welcome/Welcome.tsx
@@ -1,12 +1,23 @@
-import { Button, Stack, Text } from '@mantine/core';
-
+import { AppShell, Button, Stack, Text } from '@mantine/core';
+import { Navbar, Header } from '@mantine/core';
 export function Welcome() {
   return (
+    <AppShell
+    padding="md"
+      navbar={<Navbar width={{ base: 300 }} height={500} p="xs">{/* Navbar content */}</Navbar>}
+      header={<Header height={60} p="xs">{/* Header content */}</Header>}
+      styles={(theme) => ({
+        main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+      })}
+>    
+
     <Stack align="center" mt={50}>
       <Text size="xl" weight={500}>
         Welcome to Mantine!
       </Text>
       <Button>Click the button</Button>
     </Stack>
+    </AppShell>
+   
   );
 }
