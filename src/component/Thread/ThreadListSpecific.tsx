38fe49b37e41263e5306446b0c30c004e7e79b71
2ood <kyungmin.official0@gmail.com>
2023-04-30 08:43:21 +0900

    data structure of threadlistspecific

--- src/component/Thread/ThreadListSpecific.tsx
+++ src/component/Thread/ThreadListSpecific.tsx
@@ -7,14 +7,22 @@ const Wrap = styled.div`
   margin: 0 var(--min-padding);
 `;
 
-export default function ThreadListSpecific() {
+interface ThreadListProps {
+    content : string[];
+}
+
+export default function ThreadListSpecific({content}:ThreadListProps) {
     return (
         <Wrap>
             <ThreadStart />
-            <ThreadRingConnection />
-            <ThreadContent content="할 건 많고, 시간은 부족하네요..ㅠㅠ 시간이 참 속절없습니다" />
-            <ThreadRingConnection />
-            <ThreadContent content="저도 완전 공감이에요..."/>
+            {content.map((element, index)=>{
+	            return (
+                    <>
+                        <ThreadRingConnection />
+                        <ThreadContent key={index} content={element} />
+                    </>
+                );})
+            }
         </Wrap>
     );
 }
