43889b59afd71c0001e0a5faa64d931882634567
2ood <kyungmin.official0@gmail.com>
2023-04-30 00:45:59 +0900

    font and colors

--- src/component/UnderlinedText.tsx
+++ src/component/UnderlinedText.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const Wrapper = styled.div`
     position : inline-block;
     text-decoration : underline;
+    color : var(--placeholder-font);
 `;
 
 export default function UnderlinedText(props:any) {
