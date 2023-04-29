7266834beb2cae11eb307b212c87ea77030d23a8
2ood <kyungmin.official0@gmail.com>
2023-04-29 20:56:08 +0900

    added readme, react router

---
+++ README.md
@@ -0,0 +1,15 @@
+# 서광포
+
+# 브랜치 전략
+
+* 개인 개발 브랜치 (*-dev)
+각자 브랜치를 만들어서 개발하기.
+
+* 메인 브랜치 (master)
+큰 issue 해결할 때마다 dev 브랜치에서 merge.
+merge 할 때 팀원들에게 알리기.  
+
+
+# 커밋 메시지 전략
+
+[semantic message](https://www.conventionalcommits.org/en/v1.0.0/) 를 따릅니다. 
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './view/Main/Main';
+import MyLog from './view/MyLog/MyLog';
+import WriteOrigin from './view/WriteOrigin/WriteOrigin';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Main />}></Route>
+          <Route path="/my-log" element={<MyLog />}></Route>
+          <Route path="/write-origin" element={<WriteOrigin />}></Route>
+				</Routes>
+			</BrowserRouter>
+  );
 }
-
-export default App
