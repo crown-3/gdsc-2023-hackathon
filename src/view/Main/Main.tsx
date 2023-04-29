6bb0727277381ae21930e77f8db0b866172b6807
Crown3 <strikedown7@gmail.com>
2023-04-30 00:34:01 +0900

    feat: added font and color style convention

--- src/view/Main/Main.tsx
+++ src/view/Main/Main.tsx
@@ -5,8 +5,14 @@ import { TopbarLogo } from "../../component/Topbar";
 
 const MainInputWrap = styled.div`
   margin: 0 var(--min-padding);
-  margin-top: 10px;
+  margin-top: 20px;
   height: 160px;
+`;
+
+const MainInputPlaceholder = styled.div`
+  font-family: "NanumMyeongjo";
+  font-size: var(--font-size-medium);
+  color: var(--placeholder-font);
 `;
 
 export default function Main() {
@@ -16,6 +22,14 @@ export default function Main() {
       <MainText content={"오늘의 글을"}></MainText>
       <MainText content={"아직 남기지 않으셨네요!"}></MainText>
       <MainInputWrap>
+        <div
+          style={{
+            borderTop: "1px solid var(--primary-font)",
+          }}
+        ></div>
+        <MainInputPlaceholder>
+          오늘은 어떤 일이 있으셨나요?
+        </MainInputPlaceholder>
         <div
           style={{
             borderTop: "1px solid var(--primary-font)",
