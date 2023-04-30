5a03101071a1a00efc8f086bf20dad4ca63b1d71
2ood <kyungmin.official0@gmail.com>
2023-04-30 10:34:14 +0900

    made Feed.tsx

--- src/view/Feed/Feed.tsx
+++ src/view/Feed/Feed.tsx
@@ -1,14 +1,42 @@
 import Container from "../../component/Container";
 import { TopbarLogo } from "../../component/Topbar";
-import ThreadListSpecific from "../../component/Thread/ThreadListSpecific.tsx";
+import ThreadListSpecific, { Post } from "../../component/Thread/ThreadListSpecific.tsx";
 import Navigation from "../../component/Navigation.tsx";
+import { useEffect, useState } from "react";
+import axiosInstance from "../../axiosSetting.ts";
+import axios from "axios";
+import horizontal_stars from "../../assets/horizontal-stars.png";
 
 export default function Feed() {
+    const [feed_data,setFeedData] = useState<Array<Post[]>>([]);
+
+    async function fetchPublicPosts() {
+      try {
+        const {data, status} = await axiosInstance.get(`/posts/public`);
+        setFeedData(data.postReceiveDetail);
+      } catch (e) {
+        if (axios.isAxiosError(e)) {
+          return e.message;
+        } else {
+          return "An unexpected error occurred.";
+        }
+      }
+    }
+  
+    useEffect(() => {
+      fetchPublicPosts()
+    }, [])
+  
+  console.log(feed_data);
   return (
     <Container>
       <TopbarLogo />
-      <ThreadListSpecific contents={["안녕하세요","더미데이터에요"]}/>
-      <div style={{ marginTop: "30px" }}></div>
+        {feed_data.map((posts)=>{
+          return(<>
+            <ThreadListSpecific contents={posts}/>
+            <div style={{ display:"flex",alignItems:"center",justifyContent:"center", margin:" var(--min-padding) 0"}}><img src={horizontal_stars}></img></div>
+          </>);
+      })}
       <Navigation page="feed" />
     </Container>
   );
