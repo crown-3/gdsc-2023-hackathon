8053e45e832f81fb013fc658dc222952510df729
gaeeeguri <sjpark513@gm.gist.ac.kr>
2023-04-30 10:07:48 +0900

    feat(main): add fetchDidWriteToday

--- src/view/Main/Main.tsx
+++ src/view/Main/Main.tsx
@@ -7,9 +7,35 @@ import Navigation from "../../component/Navigation";
 import ThreeStars from "../../component/ThreeStars";
 import { getCookie } from "../../cookie";
 import { useNavigate } from "react-router-dom";
+import React, {useEffect} from "react";
+import axiosInstance from "../../axiosSetting";
+import axios from "axios";
 
 export default function Main() {
   const navigate = useNavigate();
+
+  const [didWriteToday, setDidWriteToday] = React.useState<boolean>(false);
+
+  const fetchDidWrite = async () => {
+    try {
+      const {data} = await axiosInstance.get('/user/today');
+      setDidWriteToday(data);
+    } catch (e) {
+      if (axios.isAxiosError(e)) {
+      console.log("error message: ", e.message);
+      return e.message;
+    } else {
+      console.log("unexpected error: ", e);
+      return "An unexpected error occurred.";
+      }
+    }
+  }
+
+  useEffect(() => {
+    fetchDidWrite();
+  }, []);
+
+
 
   if (getCookie("accessToken") == undefined) {
     console.log("no cookie");
@@ -19,8 +45,20 @@ export default function Main() {
   return (
     <Container>
       <TopbarLogo />
-      <MainText content={"오늘의 글을"}></MainText>
-      <MainText content={"아직 남기지 않으셨네요!"}></MainText>
+      {
+        !didWriteToday &&
+        <>
+          <MainText content={"오늘의 글을"}></MainText>
+          <MainText content={"아직 남기지 않으셨네요!"}></MainText>
+        </>
+      }
+      {
+        didWriteToday &&
+        <>
+          {/* TODO: 추가 바람*/}
+          <div>오늘은 이미 글을 작성했습니다!</div>
+        </>
+      }
       <span
         onClick={() => {
           navigate("/write-origin");
