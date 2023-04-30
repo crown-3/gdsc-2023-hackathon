90fd654d9abb9275c6b0acd976482d4d94010d5e
Crown3 <strikedown7@gmail.com>
2023-04-30 10:44:19 +0900

    feat:added feed

--- src/component/Thread/ThreadListSpecific.tsx
+++ src/component/Thread/ThreadListSpecific.tsx
@@ -2,19 +2,27 @@ import styled from "styled-components";
 import ThreadStart from "./ThreadStart";
 import ThreadContent from "./ThreadContent";
 import ThreadRingConnection from "./ThreadRingConnection";
+import { INode } from "../../view/Main/Main";
 
 const Wrap = styled.div`
   margin: 0 var(--min-padding);
 `;
 
-export default function ThreadListSpecific() {
-    return (
-        <Wrap>
-            <ThreadStart />
-            <ThreadRingConnection />
-            <ThreadContent content="할 건 많고, 시간은 부족하네요..ㅠㅠ 시간이 참 속절없습니다" />
-            <ThreadRingConnection />
-            <ThreadContent content="저도 완전 공감이에요..."/>
-        </Wrap>
-    );
+interface IProps {
+  thread: INode[];
+}
+
+export default function ThreadListSpecific({ thread }: IProps) {
+  console.log("sdf");
+  return (
+    <Wrap>
+      <ThreadStart />
+      {thread.map((node, index) => (
+        <span key={index}>
+          <ThreadRingConnection />
+          <ThreadContent content={node.postContent} />
+        </span>
+      ))}
+    </Wrap>
+  );
 }
--- src/view/Feed/Feed.tsx
+++ src/view/Feed/Feed.tsx
@@ -2,14 +2,51 @@ import Container from "../../component/Container";
 import { TopbarLogo } from "../../component/Topbar";
 import ThreadListSpecific from "../../component/Thread/ThreadListSpecific.tsx";
 import Navigation from "../../component/Navigation.tsx";
+import { useEffect, useState } from "react";
+import axiosInstance from "../../axiosSetting";
+import { IThread } from "../Main/Main.tsx";
+import { getCookie } from "../../cookie.ts";
+import axios from "axios";
 
 export default function Feed() {
+  const [data, setData] = useState<IThread>([[]]);
+
+  useEffect(() => {
+    async function getAllPosts() {
+      try {
+        const token = getCookie("accessToken");
+
+        const { data, status } = await axiosInstance.get<{
+          postReceiveDetail: IThread;
+        }>(`/posts/public`, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+
+        console.log("data: ", data);
+        setData(data.postReceiveDetail);
+      } catch (e) {
+        if (axios.isAxiosError(e)) {
+          console.log("error message: ", e.message);
+          setData([[]]);
+        } else {
+          console.log("unexpected error: ", e);
+          setData([[]]);
+        }
+      }
+    }
+
+    getAllPosts();
+  }, []);
+
   return (
     <Container>
       <TopbarLogo />
-      <ThreadListSpecific />
-      <div style={{ marginTop: "30px" }}></div>
-      <ThreadListSpecific />
+      {data.map((thread, index) => {
+        <span key={index}>
+          <ThreadListSpecific thread={thread} />
+          <div style={{ marginTop: "30px" }}></div>
+        </span>;
+      })}
       <Navigation page="feed" />
     </Container>
   );
